99ff9a02791d5e3209a6c617a49fc4adc6431f11
amit24k <47235028+amit24k@users.noreply.github.com>
2021-03-23 23:47:27 -0400

    server

--- app.js
+++ app.js
@@ -18,9 +18,12 @@ app.use(bodyParser.json());
 slackEvents.on('message', async (event) => {
   // console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
   console.log(event);
-  if(event.bot_id == undefined)
+  if(event.bot_id == undefined){
     const res = await webClient.chat.postMessage({text:"hi",channel: event.channel});
-   console.log('Message sent: ', res);
+    console.log('Message sent: ', res);
+  }
+
+
 });
 
 
