f3184f3f71a6980402ae6f85c833a9c5e5de63d0
amit24k <47235028+amit24k@users.noreply.github.com>
2021-03-23 22:37:26 -0400

    server

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const port = process.env.PORT || 3000;
 const app = express();
 const { createEventAdapter } = require('@slack/events-api');
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+
+app.use('/slack/events', slackEvents.expressMiddleware())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Starts server
-// app.listen(port, function() {
-//   console.log('Bot is listening on port ' + port)
-// });
-
 slackEvents.on('message', (event) => {
   console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+  return{
+    text: "Hi I am ASQ"
+  }
 });
 
 
-slackEvents.start(port).then(() => {
-  // Listening on path '/slack/events' by default
-  console.log(`server listening on port ${port}`);
+// Starts server
+app.listen(port, function() {
+  console.log('Bot is listening on port ' + port)
 });
